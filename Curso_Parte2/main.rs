3ef67f19ecc372ff85b614c66b7f43455ef6beae
diogjunior100 <diogjunior10071@gmail.com>
2023-06-02 17:23:33 -0300

    adiciona curso_parte2

---
+++ Curso_Parte2/main.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let notas: [f32; 4] = [6.5; 4];
+    let inteiro: usize = 0;
+
+    println!("{}", notas[inteiro]);
+
+    for indice in 0..notas.len() {
+        println!("A nota {} é = {}", indice + 1, notas[indice]);
+    }
+
+    matriz();
+
+    println!("{}", eh_fim_de_semana(DiaDaSemana::Quarta));
+}
+
+enum DiaDaSemana {
+    Domingo,
+    Segunda,
+    Quarta,
+    Quinta,
+    Sexta,
+    Sabado
+}
+
+fn eh_fim_de_semana(dia_da_semana: DiaDaSemana) -> bool {
+    match dia_da_semana {
+        DiaDaSemana::Domingo | DiaDaSemana::Sabado => true,
+        _ => false
+    }
+}
+
+fn matriz() {
+    let matriz: [[f32; 3]; 2] = [
+        [0.0, 1.2, 0.1],
+        [1.3, 0.3, 1.4]
+    ];
+
+    for linha in matriz {
+        for item in linha {
+            println!("Item = {}", item);
+        }
+    }
+}
+
+fn lerArquivo(caminho_arquivo: String) -> Option<String> {
+
+}
